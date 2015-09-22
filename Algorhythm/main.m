9ba6c7f2f2cfb885686b4d03f7783b52c61997aa
Adam Goldberg <adammgoldberg@hotmail.com>
2015-09-22 14:38:57 -0400

    Initial Commit

---
+++ Algorhythm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Algorhythm
+//
+//  Created by Adam Goldberg on 2015-09-22.
+//  Copyright (c) 2015 Adam Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
